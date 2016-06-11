cf1dfbf936463db87f35b7b91b2afc4f1234d06c
prabhath6 <prabhathkiran20@gmail.com>
2016-06-11 12:53:51 -0700

    add CPU usage end point
    add endpoint to return all cpu usage

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def get_cpu_times_metrics():
     return jsonify({'cpu_time': metrics.cpu_times()})
 
 
+@app.route('/cpu/usage', methods=['GET'])
+def get_cpu_usage_metrics():
+
+    return jsonify({'cpu_time': metrics.cpu_usage()})
+
+
 if __name__ == '__main__':
     app.run()
