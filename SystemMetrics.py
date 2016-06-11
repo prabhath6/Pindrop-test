264c4831ad05b9eb1ec9c4b57e2d68df86b9f232
prabhath6 <prabhathkiran20@gmail.com>
2016-06-11 12:52:31 -0700

    Add Cpu usage
    added function to get cpu usage

--- SystemMetrics.py
+++ SystemMetrics.py
@@ -3,6 +3,10 @@ import psutil
 
 class SystemMetrics:
 
+    def __init__(self):
+
+        self._cpu_metrics = dict()
+
     def cpu_times(self):
 
         cpu_times_metrics = {}
@@ -14,7 +18,16 @@ class SystemMetrics:
 
         return cpu_times_metrics
 
+    def cpu_usage(self):
+
+        cpu_usage_metrics = dict()
+        cpu_usage_metrics['cpu_count'] = psutil.cpu_count(logical=True)
+        cpu_usage_metrics['cpu_percent'] = psutil.cpu_percent(interval=1)
+        cpu_usage_metrics['cpu_usage_each_core'] = psutil.cpu_percent(interval=1, percpu=True)
+
+        return cpu_usage_metrics
+
 
 if __name__ == "__main__":
     metrics = SystemMetrics()
-    print(metrics.cpu_times())
+    print(metrics.cpu_usage())
