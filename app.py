d5961fa3f71dcacc6dfccb9962b657d0c7e6db32
prabhath6 <prabhathkiran20@gmail.com>
2016-06-11 12:38:29 -0700

    Add CPU times endpoint
    Added CPU times end point to return cpu time specific to mac.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, jsonify
+from SystemMetrics import SystemMetrics
 
 app = Flask(__name__)
+
+metrics = SystemMetrics()
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return jsonify({'message': 'Hello World'})
+
+
+@app.route('/cpu/times', methods=['GET'])
+def get_cpu_times_metrics():
+
+    return jsonify({'cpu_time': metrics.cpu_times()})
 
 
 if __name__ == '__main__':
